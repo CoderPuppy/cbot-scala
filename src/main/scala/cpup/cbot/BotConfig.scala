5142be71fff97afb6acdc85635276cc90e7692e2
CoderPuppy <cpup@cpup-laptop>
2014-04-24 17:06:16 -0400

    Add nickserv registration

--- src/main/scala/cpup/cbot/BotConfig.scala
+++ src/main/scala/cpup/cbot/BotConfig.scala
@@ -5,16 +5,20 @@ import org.pircbotx.{PircBotX, Configuration}
 case class BotConfig(realName: String,
                      username: String,
                      server: String,
+                     serverPass: String,
                      port: Int,
-                     autoReconnect: Boolean) {
+                     autoReconnect: Boolean,
+                     nickServPass: String) {
 	def isValid = realName != null &&
 	              username != null &&
 	              server != null
 
 	def pConfig(pConfig: Configuration.Builder[PircBotX]) {
 		pConfig.setServer(server, port)
+		pConfig.setServerPassword(serverPass)
 		pConfig.setName(username)
 		pConfig.setRealName(realName)
+		pConfig.setNickservPassword(nickServPass)
 	}
 }
 
@@ -22,9 +26,21 @@ object BotConfig {
 	class Builder {
 		var realName: String = null
 		var username: String = null
+		var serverPass: String = null
 		var server: String = null
 		var port: Int = 6667
 		var autoReconnect = true
+		var nickServPass: String = null
+
+		def setServerPass(newVal: String) = {
+			serverPass = newVal
+			this
+		}
+
+		def setNickServPass(newVal: String) = {
+			nickServPass = newVal
+			this
+		}
 
 		def setRealName(newVal: String) = {
 			realName = newVal
@@ -77,6 +93,6 @@ object BotConfig {
 			username = name
 		}
 
-		def finish = BotConfig(realName, username, server, port, autoReconnect)
+		def finish = BotConfig(realName, username, server, serverPass, port, autoReconnect, nickServPass)
 	}
 }
