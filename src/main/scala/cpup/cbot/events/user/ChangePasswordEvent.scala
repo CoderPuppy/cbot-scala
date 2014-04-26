9f1aa36f22a1330ac6464656642bb76f33702d8c
CoderPuppy <cpup@cpup-laptop>
2014-04-25 23:15:25 -0400

    Make ChangePasswordEvent extends UserUpdateEvent That's why my password wasn't saved

--- src/main/scala/cpup/cbot/events/user/ChangePasswordEvent.scala
+++ src/main/scala/cpup/cbot/events/user/ChangePasswordEvent.scala
@@ -2,8 +2,7 @@ package cpup.cbot.events.user
 
 import cpup.cbot.CBot
 import cpup.cbot.users.User
-import cpup.cbot.events.Event
 
-case class ChangePasswordEvent(bot: CBot, user: User, oldPassword: String, newPassword: String) extends Event with UserEvent {
+case class ChangePasswordEvent(bot: CBot, user: User, oldPassword: String, newPassword: String) extends UserUpdateEvent {
 	override def context = bot
 }
